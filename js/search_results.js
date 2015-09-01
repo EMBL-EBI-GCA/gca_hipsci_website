5413521634ed941c4858618c2205b14af3fdbee6
streeter <streeter@ebi.ac.uk>
2015-09-01 07:57:16 +0100

    update the search when the url changes

--- js/search_results.js
+++ js/search_results.js
@@ -7,30 +7,34 @@ var dependencies = [
 
 var app = angular.module('searchResults', dependencies);
 
-
-app.controller('SearchCtrl', ['$location', '$http', function($location, $http) {
+app.controller('SearchCtrl', ['$location', '$http', '$scope', function($location, $http, $scope) {
     var controller = this
-    controller.searchPhrase = $location.search().q;
     controller.hits = [];
-    if (controller.searchPhrase) {
-        var postBody = {
-            _source: ['url', 'title'],
-            query: {
-                match_phrase: {
-                    content: controller.searchPhrase
-                }
-            },
-            highlight: {
-                fields: {
-                    content: {}
+
+    var search = function() {
+        controller.searchPhrase = $location.search().q;
+        if (controller.searchPhrase) {
+            var postBody = {
+                _source: ['url', 'title'],
+                query: {
+                    match_phrase: {
+                        content: controller.searchPhrase
+                    }
                 },
-                pre_tags: ['<em><strong>'], post_tags: ['</strong></em>']
+                highlight: {
+                    fields: {
+                        content: {}
+                    },
+                    pre_tags: ['<em><strong>'], post_tags: ['</strong></em>']
+                }
             }
+            $http.post('/lines/api/sitemap/_search', postBody).then(
+                function(response) {
+                    controller.hits = response.data.hits.hits;
+                }
+            );
         }
-        $http.post('/lines/api/sitemap/_search', postBody).then(
-            function(response) {
-                controller.hits = response.data.hits.hits;
-            }
-        );
-    }
+    };
+    $scope.$on('$locationChangeStart', search);
+    search();
 }]);
