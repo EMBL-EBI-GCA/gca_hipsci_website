98180649adf9db0b6bbdbc0bcd61514641809cab
streeter <streeter@ebi.ac.uk>
2015-09-16 09:39:58 +0100

    # This is a combination of 5 commits. # The first commit's message is: search on cell lines as well as site pages
    # The 2nd commit message will be skipped:
    
    #	bugfix
    
    # The 3rd commit message will be skipped:
    
    #	bugfix
    
    # The 4th commit message will be skipped:
    
    #	bugfix
    
    # The 5th commit message will be skipped:
    
    #	bugfix

--- js/search_results.js
+++ js/search_results.js
@@ -9,36 +9,76 @@ var app = angular.module('searchResults', dependencies);
 
 app.controller('SearchCtrl', ['$location', '$http', '$scope', function($location, $http, $scope) {
     var controller = this
-    controller.hits = [];
+    controller.siteHits = [];
+    controller.lineHits = [];
+
+    var siteSearch = function(q) {
+        var postBody = {
+            _source: ['url', 'title'],
+            query: {
+                match_phrase: {
+                    content: q
+                }
+            },
+            highlight: {
+                fields: {
+                    content: {}
+                },
+                pre_tags: ['<em><strong>'], post_tags: ['</strong></em>']
+            }
+        }
+        $http.post('/lines/api/sitemap/_search', postBody).then(
+            function(response) {
+                controller.siteHits = response.data.hits;
+            }
+        );
+    }
+
+    var linesSearch = function(q) {
+        var postBody = {
+            query: {
+                multi_match: {
+                    query: q,
+                    fields: ['searchable.free', 'searchable.fixed^3'],
+                    type: "most_fields",
+                }
+            }
+        }
+        $http.post('/lines/api/cellLines/_search', postBody).then(
+            function(response) {
+                controller.lineHits = response.data.hits;
+            }
+        );
+    }
 
     var search = function() {
         controller.searchPhrase = $location.search().q;
         if (controller.searchPhrase) {
-            var postBody = {
-                _source: ['url', 'title'],
-                query: {
-                    match_phrase: {
-                        content: controller.searchPhrase
-                    }
-                },
-                highlight: {
-                    fields: {
-                        content: {}
-                    },
-                    pre_tags: ['<em><strong>'], post_tags: ['</strong></em>']
-                }
-            }
-            $http.post('/lines/api/sitemap/_search', postBody).then(
-                function(response) {
-                    controller.hits = response.data.hits.hits;
-                }
-            );
+            siteSearch(controller.searchPhrase)
+            linesSearch(controller.searchPhrase)
         }
         else {
-            controller.hits = [];
+            controller.siteHits = [];
+            controller.lineHits = [];
         }
     };
 
     $scope.$on('$locationChangeStart', search);
     search();
 }]);
+
+
+app.directive('lineHits', function() {
+    return {
+        restrict: 'E',
+        scope: 'false',
+        template: 
+  '<div>'
++ '<h3><span ng-bind="SearchCtrl.lineHits.total"></span> matching cell line<span ng-if="SearchCtrl.lineHits.total > 1">s</span></h3>'
++ '<div ng-repeat="hit in SearchCtrl.lineHits.hits">'
++ '<h4><a ng-href="/lines/#/lines/{{hit._source.name}}" ng-bind="hit._source.name"></a></h4>'
++ '</div>'
++ '<a class="btn btn-default btn-block" role="button" ng-href="/lines/#/lines?q={{SearchCtrl.searchPhrase}}">View in the cell line browser</a>'
++ '</div>'
+    };
+});
