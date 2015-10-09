4679fc88e5a5f9dd060f98458ef2eb9b24350fa6
streeter <streeter@ebi.ac.uk>
2015-10-09 16:16:31 +0100

    better formatting of search results

--- js/search_results.js
+++ js/search_results.js
@@ -78,7 +78,10 @@ app.directive('lineHits', function() {
 + '<div ng-repeat="hit in SearchCtrl.lineHits.hits">'
 + '<h4><a ng-href="/lines/#/lines/{{hit._source.name}}" ng-bind="hit._source.name"></a></h4>'
 + '</div>'
-+ '<a class="btn btn-default btn-block" role="button" ng-href="/lines/#/lines?q={{SearchCtrl.searchPhrase}}">View in the cell line browser</a>'
++ '<div ng-if="SearchCtrl.lineHits.total > SearchCtrl.lineHits.hits.length">'
++ '<h4>More results</h4>'
++ '<a ng-href="/lines/#/lines?q={{SearchCtrl.searchPhrase}}">View search results in the cell line browser</a>'
++ '</div>'
 + '</div>'
     };
 });
