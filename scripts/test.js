877fd6a36f58849c39e6096e56112743e1147945
apax18 <amirp@ebi.ac.uk>
2019-12-04 15:40:44 +0000

    test only

--- scripts/test.js
+++ scripts/test.js
@@ -14,7 +14,7 @@ function ebiFrameworkCreateDataProtectionBanner() {
 
   banner.id = "data-protection-banner";
   banner.className = "data-protection-banner";
-  banner.style.cssText = "position: absolute; background: #111; width: 50%; padding: .75rem 1%; left: 10px; top:10px; color: #eee; z-index: 10;";
+  banner.style.cssText = "position: absolute; background: #111; width: 50%; padding: .75rem 1%; left: 10; top:10; color: #eee; z-index: 10;";
   wrapper.className = "row";
   wrapper.innerHTML = "" +
     "<div class='columns medium-8 large-9 white-color'>" +
